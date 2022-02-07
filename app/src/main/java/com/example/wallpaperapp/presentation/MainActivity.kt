b4e791296b63861ee3b68116e87b196ca42e76e0
mamagulashvili16 <giorgi.mamagulashvili.1@btu.edu.ge>
2022-02-07 13:18:09 +0400

    added api

--- app/src/main/java/com/example/wallpaperapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/wallpaperapp/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.wallpaperapp
+package com.example.wallpaperapp.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.wallpaperapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
