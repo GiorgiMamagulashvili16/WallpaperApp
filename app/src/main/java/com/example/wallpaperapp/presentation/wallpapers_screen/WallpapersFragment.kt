f13b7c49400c917834a675a5e18048a3231ed0b0
mamagulashvili16 <giorgi.mamagulashvili.1@btu.edu.ge>
2022-02-08 15:14:37 +0400

    clean up fragment

--- app/src/main/java/com/example/wallpaperapp/presentation/wallpapers_screen/WallpapersFragment.kt
+++ app/src/main/java/com/example/wallpaperapp/presentation/wallpapers_screen/WallpapersFragment.kt
@@ -1,15 +1,11 @@
 package com.example.wallpaperapp.presentation.wallpapers_screen
 
-import android.app.WallpaperManager
-import android.util.Log.d
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.GridLayoutManager
-import com.bumptech.glide.Glide
 import com.example.wallpaperapp.databinding.WallpapersFragmentBinding
 import com.example.wallpaperapp.presentation.base.BaseFragment
 import com.example.wallpaperapp.presentation.base.Inflate
 import com.example.wallpaperapp.presentation.wallpapers_screen.adapters.WallpapersAdapter
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import kotlin.reflect.KClass
@@ -27,23 +23,23 @@ class WallpapersFragment : BaseFragment<WallpapersFragmentBinding, WallpapersVie
         observeWallpapers(viewModel)
     }
 
+
     private fun observeWallpapers(viewModel: WallpapersViewModel) {
         viewLifecycleOwner.lifecycleScope.launch {
             viewModel.wallpapersScreenState.collect {
                 when (it) {
                     is WallpapersScreenStates.Success -> {
                         initRecyclerView()
-                        d("ERRORSTATE", "${it.data}")
                         wallpapersAdapter.submitList(
                             it.data
                         )
                     }
                     is WallpapersScreenStates.Error -> {
-                        d("ERRORSTATE", "${it.message}")
                     }
                     is WallpapersScreenStates.Loading -> {
 
                     }
+                    else -> Unit
                 }
             }
         }
@@ -53,14 +49,6 @@ class WallpapersFragment : BaseFragment<WallpapersFragmentBinding, WallpapersVie
         with(binding.imagesRecyclerView) {
             layoutManager = GridLayoutManager(requireContext(), 2)
             adapter = wallpapersAdapter
-        }
-        wallpapersAdapter.onImageClick = {
-            viewLifecycleOwner.lifecycleScope.launch(Dispatchers.IO) {
-                WallpaperManager.getInstance(requireContext()).setBitmap(
-                    Glide.with(requireContext()).asBitmap().load(it).submit(1080   ,1920).get()
-                )
-            }
-
         }
     }
 }
