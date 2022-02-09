d81cbdc86df5d2aaff053a81befba0b62e70d642
Aleksandre Galogre-Baramidze <74540578+AleksandreGalogreBaramidze@users.noreply.github.com>
2022-02-09 16:02:44 +0400

    added wallpaper set functions

--- app/src/main/java/com/example/wallpaperapp/presentation/detail_screen/DetailViewModel.kt
+++ app/src/main/java/com/example/wallpaperapp/presentation/detail_screen/DetailViewModel.kt
@@ -1,7 +1,20 @@
 package com.example.wallpaperapp.presentation.detail_screen
 
+import android.app.WallpaperManager
+import android.content.Context
+import android.graphics.Bitmap
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.lifecycle.ViewModel
 
 class DetailViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    @RequiresApi(Build.VERSION_CODES.N)
+    fun setImageAtLockScreen(bitmap: Bitmap, context: Context){
+        val wallpapersFragment = WallpaperManager.getInstance(context)
+        wallpapersFragment.setBitmap(bitmap,null, true, WallpaperManager.FLAG_LOCK)
+    }
+    fun setImageAtHomeScreen(bitmap: Bitmap, context: Context){
+        val wallpapersFragment = WallpaperManager.getInstance(context)
+        wallpapersFragment.setBitmap(bitmap)
+    }
 }
